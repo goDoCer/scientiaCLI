df4edb2ed0d62954cdd69703ff37553213ea22cd
Pranav Bansal <prnvbn@gmail.com>
2022-03-28 14:52:18 +0100

    REMOVE dead code

--- scientia/api.go
+++ scientia/api.go
@@ -14,25 +14,3 @@ var httpClient = &http.Client{
 const (
 	baseURL = "https://api-materials.doc.ic.ac.uk/"
 )
-
-type File struct {
-	Name     string
-	path     string
-	children []*File
-	searched bool
-	isFolder bool
-}
-
-func GetFiles() []File {
-	return []File{}
-}
-
-func (file *File) Download() error {
-	if !file.searched {
-		//Perform get request
-	}
-	if !file.isFolder {
-		return nil
-	}
-	return nil
-}
