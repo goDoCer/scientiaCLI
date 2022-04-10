8152f13522044515c4754fe39e3735c610def50d
Pranav <pb719@ic.ac.uk>
2022-04-10 20:09:33 +0100

    UPDATE module name

--- install.sh
+++ install.sh
@@ -1,30 +1,30 @@
 go build
-sudo mkdir -p /usr/local/bin/scientia-cli
-sudo mv scientia-cli /usr/local/bin/scientia-cli/
+sudo mkdir -p /usr/local/bin/scientiaCLI
+sudo mv scientiaCLI /usr/local/bin/scientiaCLI/
 
-sudo touch /usr/local/bin/scientia-cli/token.txt  
-sudo chmod -R 777 /usr/local/bin/scientia-cli/token.txt
+sudo touch /usr/local/bin/scientiaCLI/token.txt  
+sudo chmod -R 777 /usr/local/bin/scientiaCLI/token.txt
 
-sudo cp ./default-config.json /usr/local/bin/scientia-cli/config.json
-sudo chmod -R 777 /usr/local/bin/scientia-cli/config.json
+sudo cp ./default-config.json /usr/local/bin/scientiaCLI/config.json
+sudo chmod -R 777 /usr/local/bin/scientiaCLI/config.json
 
-sudo cp autocomplete.sh /etc/bash_completion.d/scientia-cli
-sudo chmod +x /etc/bash_completion.d/scientia-cli
+sudo cp autocomplete.sh /etc/bash_completion.d/scientiaCLI
+sudo chmod +x /etc/bash_completion.d/scientiaCLI
 
 # NOT SURE IF WE NEED THIS?
 # echo "[ -r /usr/share/bash-completion/bash_completion ] && . /usr/share/bash-completion/bash_completion" >> ~/.bashrc
 
 echo "Scientia installed successfully"
-echo "Scientia is now available in /usr/local/bin/scientia-cli"
-# echo "export PATH=\$PATH:/usr/local/bin/scientia-cli" >> ~/.zshrc
+echo "Scientia is now available in /usr/local/bin/scientiaCLI"
+# echo "export PATH=\$PATH:/usr/local/bin/scientiaCLI" >> ~/.zshrc
 
 # UNCOMMENT FOR NORMAL BASH 
-# echo "export PATH=\$PATH:/usr/local/bin/scientia-cli" >> ~/.bashrc
+# echo "export PATH=\$PATH:/usr/local/bin/scientiaCLI" >> ~/.bashrc
 
 # UNCOMMENT FOR ZSH 
-# echo "export PATH=\$PATH:/usr/local/bin/scientia-cli" >> ~/.zshrc
+# echo "export PATH=\$PATH:/usr/local/bin/scientiaCLI" >> ~/.zshrc
 
 # UNCOMMENT FOR FISH 
 # set -U fish_user_paths /usr/local/go/bin $fish_user_paths
 
-# sudo wget -O /etc/bash_completion.d/scientia-cli https://raw.githubusercontent.com/urfave/cli/master/autocomplete/bash_autocomplete && sudo chmod +x /etc/bash_completion.d/scientia-cli
+# sudo wget -O /etc/bash_completion.d/scientiaCLI https://raw.githubusercontent.com/urfave/cli/master/autocomplete/bash_autocomplete && sudo chmod +x /etc/bash_completion.d/scientiaCLI
