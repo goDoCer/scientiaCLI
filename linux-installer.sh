50c643d52799bca09f663a10f27fa4ad5395a4c2
Pranav Bansal <prnvbn@gmail.com>
2022-09-21 15:22:23 +0100

    UPDATE install dir to be .scientia-cli

--- linux-installer.sh
+++ linux-installer.sh
@@ -18,21 +18,21 @@ fi
 
 chmod +x scientia-cli
 
-INSTALL_DIR="/usr/local/bin/scientia-cli"
+INSTALL_DIR="$HOME/.scientia-cli"
 TOKEN_FILE="$INSTALL_DIR/token.txt"
 CFG_FILE="$INSTALL_DIR/config.json"
 
-sudo mkdir -p $INSTALL_DIR
-sudo mv scientia-cli $INSTALL_DIR
+mkdir -p "$INSTALL_DIR"
+mv scientia-cli "$INSTALL_DIR"
 
-sudo touch $TOKEN_FILE
-sudo chmod -R 777 $TOKEN_FILE
+touch "$TOKEN_FILE"
+chmod -R 777 "$TOKEN_FILE"
 
-if [ ! -f $CFG_FILE ]; then
-    sudo touch $CFG_FILE
-    sudo chmod -R 777 $CFG_FILE
+if [ ! -f "$CFG_FILE" ]; then
+    touch "$CFG_FILE"
+    chmod -R 777 "$CFG_FILE"
     
-    echo "$CFG" | sudo tee $CFG_FILE > /dev/null
+    echo "$CFG" | tee "$CFG_FILE" > /dev/null
 fi
 
 set +e
@@ -42,16 +42,18 @@ cat << EndOfMessage
 Add scientia-cli to your path. You can do so by running the command depending upon which shell you use
 
 # BASH
-echo "export PATH=\\\$PATH:/usr/local/bin/scientia-cli" >> ~/.bashrc && source ~/.bashrc
+echo "export PATH=\\\$PATH:$HOME/.scientia-cli" >> ~/.bashrc && source ~/.bashrc
 
 # ZSH
-echo "export PATH=\\\$PATH:/usr/local/bin/scientia-cli" >> ~/.zshrc && source ~/.zshrc
+echo "export PATH=\\\$PATH:$HOME/.scientia-cli" >> ~/.zshrc && source ~/.zshrc
 
 # FISH
-set -U fish_user_paths /usr/local/bin/scientia-cli \$fish_user_paths
+set -U fish_user_paths $HOME/.scientia-cli \$fish_user_paths
 
 ======================================================================================================
 EndOfMessage
 
-echo "Scientia installed successfully"
-echo "Scientia is now available in /usr/local/bin/scientia-cli"
+echo "scientia-cli installed successfully"
+echo "scientia-cli is now installed at $INSTALL_DIR"
+
+echo "to uninstall run rm -r $INSTALL_DIR at any time"
